1626db050ab6d0998139e8194981e30eb2618f5a
Julien Verneaut <jverneaut@gmail.com>
2020-03-05 07:54:44 +0100

    Add github link

--- global.js
+++ global.js
@@ -1,3 +1,5 @@
+import githubLogoSrc from './github.svg';
+
 // All of this is very ugly, please don't judge me
 const analytics = document.createElement('script');
 analytics.src = 'https://www.googletagmanager.com/gtag/js?id=UA-120978536-6';
@@ -25,6 +27,7 @@ if (window.location.pathname !== '/') {
         height: 100vh;
         width: 100vw;
         display: flex;
+        flex-direction: column;
         align-items: center;
         justify-content: center;
       }
@@ -39,6 +42,13 @@ if (window.location.pathname !== '/') {
         justify-content: center;
         box-shadow: 0px 2px 6px rgba(0, 0, 0, 0.16);
       }
+
+      a.github {
+        background: black;
+        margin-top: 16px;
+        height: 48px;
+        width: 48px;
+      }
     </style>
 
     <a href="${window.location.origin}" target="_parent">
@@ -48,6 +58,13 @@ if (window.location.pathname !== '/') {
         <rect width="24" height="2" y="14"></rect>
       </svg>
     </a>
+
+    <a class="github" href="https://github.com/jverneaut/laboratoire/tree/master${
+      window.location.pathname
+    }" target="_parent">
+      <img src="${window.location.origin +
+        githubLogoSrc}" height="20" width="20">
+    </a>
   `;
 
   const iframe = document.createElement('iframe');
@@ -55,7 +72,7 @@ if (window.location.pathname !== '/') {
 
   Object.assign(iframe.style, {
     width: '92px',
-    height: '92px',
+    height: '156px',
     position: 'fixed',
     border: 'none',
     top: 0,
