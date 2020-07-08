23aa39ccbbcd81917e925f866597921cdbb9bea9
Julien Verneaut <jverneaut@gmail.com>
2020-07-08 18:49:20 +0200

    Update meta

--- app/components/App.js
+++ app/components/App.js
@@ -39,13 +39,27 @@ const App = ({ pages, categories }) => {
     <Router>
       <Helmet>
         <meta name="twitter:title" content="Le laboratoire" />
-        <meta name="og:title" content="Le laboratoire" />
         <meta
           name="twitter:image"
           content={'https://lab.julienverneaut.com' + socialImg}
         />
+        <meta property="og:url" content="https://lab.julienverneaut.com" />
+        <meta property="og:title" content="Le laboratoire" />
+        <meta
+          property="og:description"
+          content="Ce site rassemble mes expérimentations frontend. Il contient des essais d'animations, des expériences avec WebGL, des essais d'api et bien d'autres choses."
+        />
+        <meta property="og:image:type" content="image/png" />
         <meta
           property="og:image"
+          content={'https://lab.julienverneaut.com' + socialImg}
+        />
+        <meta
+          property="og:image:url"
+          content={'https://lab.julienverneaut.com' + socialImg}
+        />
+        <meta
+          property="og:image:secure_url"
           content={'https://lab.julienverneaut.com' + socialImg}
         />
       </Helmet>
