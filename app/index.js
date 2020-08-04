49cee45030d85047ae9599ca829cff30807d72cd
Julien Verneaut <jverneaut@gmail.com>
2020-08-04 12:27:45 +0200

    Fix social image

--- app/index.js
+++ app/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
 
+import './social.png';
 import './main.scss';
 
 if (document.querySelector('#root').childElementCount) {
