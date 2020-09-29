e8e137442c6abd7fb87671c101cd5d71229723c1
Julien Verneaut <jverneaut@gmail.com>
2020-09-29 23:03:58 +0200

    Add fluid

---
+++ src/_fluid/index.js
@@ -0,0 +1,7 @@
+// http://nielsdaemen.nl/webgl/fluid_sim_1/app.js
+// https://github.com/weymouth/lily-pad
+// http://www.nlpr.ia.ac.cn/2012papers/gjkw/gk45.pdf
+// http://developer.download.nvidia.com/books/HTML/gpugems/gpugems_ch38.html
+// http://jamie-wong.com/2016/08/05/webgl-fluid-simulation/
+// https://github.com/amandaghassaei/VortexShedding
+// https://github.com/PavelDoGreat/WebGL-Fluid-Simulation
