270efa787d140631c72b93f2b2b3c80bea262788
Julien Verneaut <jverneaut@gmail.com>
2021-12-07 19:05:30 +0100

    Add post-processing

--- src/matcap-editor/components/Preview.js
+++ src/matcap-editor/components/Preview.js
@@ -1,6 +1,13 @@
 import React, { Suspense } from 'react';
 import { Canvas } from '@react-three/fiber';
 import { OrbitControls } from '@react-three/drei';
+import {
+  EffectComposer,
+  DepthOfField,
+  Bloom,
+  Noise,
+  Vignette,
+} from '@react-three/postprocessing';
 
 import suzanne from '../suzanne.obj';
 
@@ -42,6 +49,17 @@ const Preview = ({ texture }) => {
         <Model texture={texture} />
         <OrbitControls autoRotate={true} />
       </Suspense>
+      <EffectComposer>
+        {/* <DepthOfField
+          focusDistance={0}
+          focalLength={0.02}
+          bokehScale={2}
+          height={480}
+        /> */}
+        {/* <Bloom luminanceThreshold={0} luminanceSmoothing={0.9} height={300} /> */}
+        {/* <Noise opacity={0.25} /> */}
+        {/* <Vignette eskil={false} offset={0.1} darkness={1.1} /> */}
+      </EffectComposer>
     </Canvas>
   );
 };
