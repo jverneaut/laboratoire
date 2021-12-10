a9ab86baebcf2fe4a255a3b51ded59db2ccd39b5
Julien Verneaut <jverneaut@gmail.com>
2021-12-10 11:47:33 +0100

    Update Wappalyzer

--- src/wappalyzer/index.js
+++ src/wappalyzer/index.js
@@ -1,27 +1,96 @@
-import RandExp from 'randexp';
-import data from './apps.json';
-
 import './main.scss';
 
-const apps = Object.keys(data.apps);
+import RandExp from 'randexp';
 
-apps.forEach(app => {
-  const appData = data.apps[app];
+const importAll = (r) => {
+  return r.keys().map(r);
+};
 
-  if (appData.js) {
-    const variableNames = Object.keys(appData.js);
-    variableNames.forEach(variableName => {
-      const reg = new RandExp(appData.js[variableName]);
-      window.variableName = reg.gen();
-    });
-  }
+const technologiesGroups = importAll(
+  require.context('./technologies', false, /\.json$/)
+);
 
-  if (appData.script && typeof appData.script === 'string') {
-    console.log(appData.script);
-    const script = document.createElement('script');
-    script.src = new RandExp(appData.script).gen();
-    script.type = 'javascript/blocked';
+const generateValue = (regStr) => {
+  const reg = new RandExp(regStr);
+  return reg.gen();
+};
 
-    document.body.appendChild(script);
-  }
+technologiesGroups.forEach((technologiesGroup) => {
+  Object.values(technologiesGroup).forEach((technology) => {
+    // Create cookie with key and generated value
+    if (technology.cookies) {
+      Object.keys(technology.cookies).forEach((cookieKey) => {
+        const cookieValue = generateValue(technology.cookies[cookieKey]);
+        const cookiePath = window.location.pathname;
+
+        document.cookie = `${cookieKey}=${cookieValue};path=${cookiePath}`;
+      });
+    }
+
+    // Not implemented
+    if (technology.dom) {
+    }
+
+    // Not implemented
+    if (technology.dns) {
+    }
+
+    // Pollute global scope
+    if (technology.js) {
+      Object.keys(technology.js).forEach((propertyName) => {
+        window[propertyName] = generateValue(technology.js[propertyName]);
+      });
+    }
+
+    // Not implemented
+    if (technology.headers) {
+    }
+
+    // Not implemented
+    if (technology.html) {
+    }
+
+    // Not implemented
+    if (technology.text) {
+    }
+
+    // Not implemented
+    if (technology.css) {
+    }
+
+    // Not implemented
+    if (technology.robots) {
+    }
+
+    // Not implemented
+    if (technology.url) {
+    }
+
+    // Not implemented
+    if (technology.xhr) {
+    }
+
+    // Generate meta tags
+    if (technology.meta) {
+      Object.keys(technology.meta).forEach((metaName) => {
+        const meta = document.createElement('meta');
+        meta.name = metaName;
+        meta.content = generateValue(
+          [technology.meta[metaName]].flat().join(' ')
+        );
+
+        document.head.appendChild(meta);
+      });
+    }
+
+    if (technology.scriptSrc) {
+      [technology.scriptSrc].flat().forEach((scriptSrc) => {
+        const script = document.createElement('script');
+        script.src = generateValue(scriptSrc);
+        script.type = 'javascript/blocked';
+
+        document.head.appendChild(script);
+      });
+    }
+  });
 });
