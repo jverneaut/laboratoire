85855ffb07415680bf210472823a71009ca172d4
Julien Verneaut <jverneaut@gmail.com>
2022-01-28 10:40:05 +0100

    Add api.json

--- config/base.config.js
+++ config/base.config.js
@@ -1,11 +1,12 @@
 const webpack = require('webpack');
-const { lstatSync, readdirSync, readFileSync } = require('fs');
-const { join, resolve } = require('path');
+const { lstatSync, readdirSync, readFileSync, existsSync } = require('fs');
+const { join, resolve, relative } = require('path');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const FaviconsWebpackPlugin = require('favicons-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const CopyPlugin = require('copy-webpack-plugin');
 const SitemapPlugin = require('sitemap-webpack-plugin').default;
+const GenerateJsonPlugin = require('generate-json-webpack-plugin');
 
 const isDirectory = (source) => lstatSync(source).isDirectory();
 const getDirectories = (source) =>
@@ -13,19 +14,59 @@ const getDirectories = (source) =>
     .map((name) => join(source, name))
     .filter(isDirectory);
 
-const pageDirectories = getDirectories(join(__dirname, '../src')).filter(
-  (dir) =>
+const pageDirectories = getDirectories(join(__dirname, '../src'));
+
+const getPageTitle = (path) => {
+  const html = readFileSync(path, 'utf8');
+  return html
+    .match(/<title>(.*?)<\/title>/g)[0]
+    .replace('<title>', '')
+    .replace('</title>', '');
+};
+
+const getPageCategory = (path) => {
+  const html = readFileSync(path, 'utf8');
+  return html
+    .match(/<meta name="category" content="(.*?)" \/>/g)[0]
+    .replace('<meta name="category" content="', '')
+    .replace('" />', '');
+};
+
+const getPageDate = (path) => {
+  const html = readFileSync(path, 'utf8');
+  const dateMatch = html.match(/<meta name="date" content="(.*?)" \/>/g);
+  if (!dateMatch) return Date.now();
+
+  return parseInt(
+    dateMatch[0]
+      .replace('<meta name="date" content="', '')
+      .replace('" />', '')
+      .padEnd(13, 0)
+  );
+};
+
+const pages = pageDirectories
+  .filter((dir) =>
     process.env.NODE_ENV === 'production'
       ? dir.split('/').reverse()[0].charAt(0) !== '_'
       : true
-);
+  )
+  .map((pageDirectory) => {
+    const slug = pageDirectory.split('/').reverse()[0];
+    const html = join(pageDirectory, 'index.html');
+    const js = join(pageDirectory, 'index.js');
+    const name = getPageTitle(join(pageDirectory, 'index.html'));
+    const category = getPageCategory(join(pageDirectory, 'index.html'));
+    const screenshot = join(pageDirectory, 'screenshot.png');
+    const date = getPageDate(join(pageDirectory, 'index.html')) || 'TROLOLO';
 
-const pages = pageDirectories.map((pageDirectory) => {
-  const slug = pageDirectory.split('/').reverse()[0];
-  const page = { slug };
+    const page = { slug, html, js, name, category, date };
+    if (existsSync(screenshot)) {
+      page.screenshot = relative(__dirname, screenshot);
+    }
 
-  return page;
-});
+    return page;
+  });
 
 module.exports = {
   entry: {
@@ -64,6 +105,16 @@ module.exports = {
     new CopyPlugin({
       patterns: [{ from: 'public/' }],
     }),
+    new GenerateJsonPlugin(
+      'api.json',
+      pages.map((page) => ({
+        date: page.date,
+        name: page.name,
+        category: page.category,
+        url: `https://lab.julienverneaut.com/${page.slug}`,
+        cover: `https://lab.julienverneaut.com/${page.slug}/screenshot.png`,
+      }))
+    ),
   ],
   devServer: {
     contentBase: resolve(__dirname, 'dist'),
