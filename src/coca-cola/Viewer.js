e041f44869d8106b5e7310df5ab3a1f4d26bcb44
Julien Verneaut <jverneaut@gmail.com>
2024-10-08 12:26:53 +0200

    Add particles

--- src/coca-cola/Viewer.js
+++ src/coca-cola/Viewer.js
@@ -1,17 +1,67 @@
-import React, { Suspense, useState, useEffect } from 'react';
+import React, { Suspense, useMemo, useState, useEffect } from 'react';
 import { Canvas } from '@react-three/fiber';
 import { useGLTF } from '@react-three/drei';
 
 const Model = ({ url, coords }) => {
   const { scene } = useGLTF(url);
 
+  const points = useMemo(() => {
+    const numPoints = 1200;
+    const positions = new Float32Array(numPoints * 3);
+    for (let i = 0; i < numPoints; i++) {
+      const randomSeed = Math.random() * Math.PI * 2;
+
+      const x =
+        -0.1 + Math.sin(randomSeed) * 0.3 + (Math.random() - 0.5) * 0.12;
+
+      const z = 0.05 + Math.cos(randomSeed) * 0.3 + (Math.random() - 0.5) * 0.1;
+
+      positions[i * 3] = x;
+      positions[i * 3 + 1] = (Math.random() - 0.5) * 0.5 + 0.074;
+      positions[i * 3 + 2] = z;
+    }
+    return positions;
+  }, []);
+
   return (
-    <group
-      position={[coords.position.x, coords.position.y, coords.position.z]}
-      rotation={[coords.rotation.x, coords.rotation.y, coords.rotation.z]}
-    >
-      <primitive object={scene} />
-    </group>
+    <>
+      <group
+        position={[coords.position.x, coords.position.y, coords.position.z]}
+        rotation={[coords.rotation.x, coords.rotation.y, coords.rotation.z]}
+      >
+        <primitive object={scene} />
+      </group>
+
+      <group
+        position={[
+          -0.1 * coords.position.y,
+          0.2 * coords.position.z,
+          -0.2 * coords.position.x,
+        ]}
+        rotation={[
+          0.25 * coords.rotation.x + 0.125 * coords.rotation.y,
+          0.125 * coords.rotation.y,
+          0.25 * coords.rotation.z - 0.125 * coords.rotation.x,
+        ]}
+      >
+        <points>
+          <bufferGeometry>
+            <bufferAttribute
+              attachObject={['attributes', 'position']}
+              count={points.length / 3}
+              array={points}
+              itemSize={3}
+            />
+          </bufferGeometry>
+          <pointsMaterial
+            color="white"
+            size={0.001}
+            transparent
+            opacity={0.6}
+          />
+        </points>
+      </group>
+    </>
   );
 };
 
